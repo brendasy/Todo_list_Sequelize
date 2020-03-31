4a320517e41bdb667666ea2ceefcdf7a360e372d
brendasy <simpurity@gmail>
2020-03-31 18:22:19 +0800

    add routes and views for register and login pages

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 //express-handlebars
 const exphbs = require('express-handlebars')
-exphbs('hadlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //body-parser
 const bodyParser = require('body-parser')
@@ -25,10 +25,55 @@ const passport = require('passport')
 app.use(passport.initialize())
 app.use(passport.session())
 
-
+//public
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('testtest')
+  res.render('index')
+})
+
+app.get('/todos', (req, res) => {
+  res.render('index')
+})
+
+app.get('/todos/:id', (req, res) => {
+
+})
+
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  res.render('index')
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+
+})
+
+app.put('/todos/:id', (req, res) => {
+
+})
+
+app.delete('/todos/:id', (req, res) => {
+  res.render('index')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/regester', (req, res) => {
+
 })
 
 app.listen(3000, () => {
